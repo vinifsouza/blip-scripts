e2e6cfe844f25556eec8e7b4b7fdc2352e02c4a3
Vinícius Souza <vinicius.fernando.souza@gmail.com>
2022-02-23 09:54:43 -0300

    refactor(createMenu): refactor quick reply functions

--- src/create-menu/__tests__/createMenu.dev.test.js
+++ src/create-menu/__tests__/createMenu.dev.test.js
@@ -1,4 +1,9 @@
-const { _generateTextMenu } = require('../createMenu.dev');
+const {
+    _generateTextMenu,
+    _generateBlipChatQuickReply,
+    _generateWhatsAppQuickReply,
+    __checkOptionToQuickReply
+} = require('../createMenu.dev');
 
 describe('text menu', () => {
     it('should generate text menu with numered options', () => {
@@ -15,5 +20,104 @@ describe('text menu', () => {
             'Choose an option\n\n<b>1.</b> First Option\n<b>2.</b> Second Option\n<b>3.</b> Third Option';
 
         expect(response).toStrictEqual(expected);
+    });
+});
+
+describe('generate quick reply', () => {
+    const payloadText = 'Choose an option';
+    const payloadOptions = ['First Option', 'Second Option', 'Third Option'];
+
+    it('should generate quick reply to BLIPCHAT', () => {
+        const response = _generateBlipChatQuickReply(
+            payloadText,
+            payloadOptions
+        );
+
+        const expected = {
+            type: 'application/vnd.lime.select+json',
+            content: {
+                scope: 'immediate',
+                text: payloadText,
+                options: [
+                    {
+                        order: 1,
+                        text: 'First Option'
+                    },
+                    {
+                        order: 2,
+                        text: 'Second Option'
+                    },
+                    {
+                        order: 3,
+                        text: 'Third Option'
+                    }
+                ]
+            }
+        };
+
+        expect(response).toStrictEqual(expected);
+    });
+
+    it('should generate quick reply to WHATSAPP', () => {
+        const expected = {
+            content: {
+                recipient_type: 'individual',
+                type: 'interactive',
+                interactive: {
+                    type: 'button',
+                    body: {
+                        text: payloadText
+                    },
+                    action: {
+                        buttons: [
+                            {
+                                type: 'reply',
+                                reply: {
+                                    id: 'First Option',
+                                    title: 'First Option'
+                                }
+                            },
+                            {
+                                type: 'reply',
+                                reply: {
+                                    id: 'Second Option',
+                                    title: 'Second Option'
+                                }
+                            },
+                            {
+                                type: 'reply',
+                                reply: {
+                                    id: 'Third Option',
+                                    title: 'Third Option'
+                                }
+                            }
+                        ]
+                    }
+                }
+            },
+            type: 'application/json'
+        };
+
+        const response = _generateWhatsAppQuickReply(
+            payloadText,
+            payloadOptions
+        );
+
+        expect(response).toStrictEqual(expected);
+    });
+});
+
+describe('test props and options validation', () => {
+    it('should throw new error when option length exceed the limit of 20 characters', () => {
+        const option = 'Lorem ipsum dolor sit amet'; // 26 characters
+        const messageErrorExpected = `Option '${option}' exceed the limit of 20 characters to WhatsApp quick reply`;
+        expect(() => {
+            __checkOptionToQuickReply(option);
+        }).toThrow(messageErrorExpected);
+    });
+
+    it('should return option when option length not exceed the limit of 20 characters', () => {
+        const option = 'Lorem ipsum dolor si'; // 20 characters
+        expect(__checkOptionToQuickReply(option)).toStrictEqual(option);
     });
 });
--- src/create-menu/createMenu.dev.js
+++ src/create-menu/createMenu.dev.js
@@ -27,7 +27,58 @@ function _generateTextMenu(text, options) {
     return textMenu;
 }
 
+function _generateBlipChatQuickReply(text, options) {
+    options = options.map((option, idx) => ({
+        order: idx + 1,
+        text: option
+    }));
+
+    return {
+        type: MENU_TYPES.QUICK_REPLY.BLIP_CHAT.TYPE,
+        content: {
+            scope: MENU_TYPES.QUICK_REPLY.BLIP_CHAT.SCOPE,
+            text,
+            options
+        }
+    };
+}
+
+function _generateWhatsAppQuickReply(text, options) {
+    const buttons = options.map((option) => ({
+        type: MENU_TYPES.QUICK_REPLY.WHATSAPP.BUTTON_TYPE,
+        reply: {
+            id: option,
+            title: __checkOptionToQuickReply(option)
+        }
+    }));
+
+    return {
+        type: MENU_TYPES.QUICK_REPLY.WHATSAPP.TYPE,
+        content: {
+            recipient_type: MENU_TYPES.QUICK_REPLY.WHATSAPP.RECIPIENT_TYPE,
+            type: MENU_TYPES.QUICK_REPLY.WHATSAPP.CONTENT_TYPE,
+            interactive: {
+                type: MENU_TYPES.QUICK_REPLY.WHATSAPP.INTERACTIVE_TYPE,
+                body: { text },
+                action: { buttons }
+            }
+        }
+    };
+}
+
+function __checkOptionToQuickReply(option) {
+    if (option.length > 20) {
+        throw new Error(
+            `Option '${option}' exceed the limit of 20 characters to WhatsApp quick reply`
+        );
+    }
+    return option;
+}
+
 // exports
 module.exports = {
-    _generateTextMenu
+    _generateTextMenu,
+    _generateBlipChatQuickReply,
+    _generateWhatsAppQuickReply,
+    __checkOptionToQuickReply
 };
